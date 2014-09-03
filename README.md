b76abec96d2369570bd817556c2fed9684c3ce5f
alchemyunited <mark.simchock@alchemyunited.com>
2014-09-03 07:45:32 -0400

    push #1

--- README.md
+++ README.md
@@ -1,4 +1,13 @@
-daniel-goleman-focus
-====================
+Daniel Goleman - Focus
+======================
 
-Key takeaways from the book "Focus" by Daniel Goleman. Goleman was also the author of "Emotional Intelligence."
+Key takeaways from the book "Focus" by Daniel Goleman. 
+
+Buy the book: https://www.overdrive.com/media/1223294/focus
+
+Slideshow: http://chiefalchemist.github.io/daniel-goleman-focus/
+
+Pinterest: http://www.pinterest.com/chiefalchemist/focus-daniel-goleman/
+
+
+##This is a work in progress. It's an idea (i.e., using GH in this way) that's currently in the prototype stage.
---
+++ a-edit-this/my-quotes.js
@@ -0,0 +1,44 @@
+/**
+ * Note: Please scroll to the bottom for more details. The top has been reserved for notes and such.
+ *
+ * <br> = return
+ * <br><br> = paragraph break
+ *
+ * ** If you're not familar with programming and how that relates to apostrophes, single quotes and double quotes then please use: **
+ *
+ * - apostrophe = &#39;
+ *
+ * - left single quote = &lsquo;
+ * - right single quote = &rsquo;
+ *
+ * - left double quote = &ldquo;
+ * - right double quote = &rdquo;
+ *
+ * Other special characters: http://www.degraeve.com/reference/specialcharacters.php
+ */
+ 
+var ezMQ = {
+  "ezobj":[
+
+    {
+	  'index'		: 2,
+	  'title'		: '',
+	  'idea_size'	: '135',
+	  'idea'		: "Though it matters enormously for how we navigate life, attention in all its varieties represents a little-notice and underrated mental asset. My goal here is to spotlight this elusive and under-appreciated mental faculty in the mind's operation and its role in living a fulfilling life.",	  'quotes'		: true,
+	  'select'		: 'My goal here',
+	  'who'			: 'Daniel Goleman',
+	  'note'		: 'Focus - Chapter 1',
+	  'caption'		: setupCaption
+    },
+	
+    {
+	  'index'		: 1,
+	  'title'		: "", 
+	  'idea'		: '<img src="img/cover.jpg">',
+	  'quotes'		: false,
+	  'select'		: 'Cover: "Your Brain At Work" - David Rock',
+	  'who'			: '',
+	  'note'		: '',
+	  'caption'		: setupCaption
+    }
+  ]}
